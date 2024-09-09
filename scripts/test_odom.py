4679fbeefb9a6b457b8e4ba5f6406e13b2608f36
Burak Aksoy <burakaksoy20@gmail.com>
2024-09-08 20:04:02 -0400

    Extend test odom and twist scripts to include two particles

--- scripts/test_odom.py
+++ scripts/test_odom.py
@@ -2,16 +2,29 @@
 import rospy
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Point, Quaternion
+import tf.transformations as transformations
+import numpy as np
+
 
 def publish_odometry():
     rospy.init_node('custom_odom_publisher', anonymous=True)
 
 
-    pub = rospy.Publisher('/custom_static_particles_odom_0', Odometry, queue_size=1)
+    # pub = rospy.Publisher('/custom_static_particles_odom_0', Odometry, queue_size=1)
+    pub = rospy.Publisher('/odom_particle_0', Odometry, queue_size=1)
+    pub2 = rospy.Publisher('/odom_particle_39', Odometry, queue_size=1)
     rate = rospy.Rate(100)  # 10 Hz
 
-    position = Point(-1, 0, 2)
-    orientation = Quaternion(0, 0.7071081, 0, 0.7071055)
+    # position = Point(-1, 0, 2)
+    # orientation = Quaternion(0, 0.7071081, 0, 0.7071055)
+    
+    position = Point(0.48, -0.08, 0.3)
+    position2 = Point(0, -0.08, 0.3)
+    
+    th_x, th_y,th_z = np.deg2rad([90, 0, -110])
+    th_x2, th_y2,th_z2 = np.deg2rad([90, 0, -70])
+    orientation = Quaternion(*transformations.quaternion_from_euler(th_x, th_y,th_z))
+    orientation2 = Quaternion(*transformations.quaternion_from_euler(th_x2, th_y2,th_z2))
 
     # Time tracking
     start_time = rospy.Time.now()
@@ -20,9 +33,9 @@ def publish_odometry():
     # Phase durations in seconds
     max_velocity = 1
     zero_velocity_duration = 4
-    ramp_up_duration = 1
-    constant_velocity_duration = 2
-    ramp_down_duration = 1
+    ramp_up_duration = 0.1
+    constant_velocity_duration = 0.4
+    ramp_down_duration = 0.1
     ramp_down_start = zero_velocity_duration + ramp_up_duration + constant_velocity_duration
 
     while not rospy.is_shutdown():
@@ -47,6 +60,7 @@ def publish_odometry():
 
         # Position integration
         position.x += velocity * elapsed_time
+        position2.x += velocity * elapsed_time
 
         # Prepare and publish Odometry message
         odom_msg = Odometry()
@@ -58,6 +72,14 @@ def publish_odometry():
         odom_msg.twist.twist.linear.x = velocity
 
         pub.publish(odom_msg)
+        
+        odom_msg2 = odom_msg
+        odom_msg2.pose.pose.position = position2
+        odom_msg2.pose.pose.orientation = orientation2
+        odom_msg2.twist.twist.linear.x = velocity
+        
+        pub2.publish(odom_msg2)
+        
         last_time = current_time
         rate.sleep()
 
--- scripts/test_twist.py
+++ scripts/test_twist.py
@@ -7,7 +7,10 @@ def publish_velocity():
     rospy.init_node('custom_velocity_publisher', anonymous=True)
 
     # Publisher for Twist messages
-    pub = rospy.Publisher('/custom_static_particles_cmd_vel_0', Twist, queue_size=1)
+    # pub = rospy.Publisher('/custom_static_particles_cmd_vel_0', Twist, queue_size=1)
+    pub = rospy.Publisher('/cmd_vel_particle_0', Twist, queue_size=1)
+    pub2 = rospy.Publisher('/cmd_vel_particle_39', Twist, queue_size=1)
+    
     rate = rospy.Rate(100)  # 10 Hz
 
     # Time tracking
@@ -17,9 +20,9 @@ def publish_velocity():
     # Phase durations in seconds
     max_velocity = 1
     zero_velocity_duration = 4
-    ramp_up_duration = 1
-    constant_velocity_duration = 2
-    ramp_down_duration = 1
+    ramp_up_duration = 0.1
+    constant_velocity_duration = 0.4
+    ramp_down_duration = 0.1
     ramp_down_start = zero_velocity_duration + ramp_up_duration + constant_velocity_duration
 
     while not rospy.is_shutdown():
@@ -47,6 +50,7 @@ def publish_velocity():
         # Set angular velocities if needed, e.g., twist_msg.angular.z = angular_velocity
 
         pub.publish(twist_msg)
+        pub2.publish(twist_msg)
         last_time = current_time
         rate.sleep()
 
